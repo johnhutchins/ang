73e3b1e891087a10b2ed357e66d1271e96c22c9e
John Hutchins <john@aseba.org>
2018-02-27 14:17:30 -0500

    add back button

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 import {IProduct} from "./product";
 
@@ -11,7 +11,11 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
